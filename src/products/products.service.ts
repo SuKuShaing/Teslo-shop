4949925b77539b183c89c141de8a0b6a7f7f6587
Sebastian <sebastian.sanhueza.t@usach.cl>
2026-07-02 14:08:52 -0400

    Primera inserción mediante typeorm

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
-  }
+	constructor(
+		@InjectRepository(Product)
+		private readonly productRepository: Repository<Product>,
+	) {}
 
-  findAll() {
-    return `This action returns all products`;
-  }
+	async create(createProductDto: CreateProductDto) {
+		try {
+			const product = this.productRepository.create(createProductDto);
+			await this.productRepository.save(product);
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
-  }
+			return product;
+		} catch (error) {
+			console.log(error); // Esto lo dejamos en el servidor
+			throw new InternalServerErrorException('Ayuda!!!'); // Esto lo ve el usuario
+		}
+	}
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
+	findAll() {
+		return `This action returns all products`;
+	}
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
-  }
+	findOne(id: number) {
+		return `This action returns a #${id} product`;
+	}
+
+	update(id: number, updateProductDto: UpdateProductDto) {
+		return `This action updates a #${id} product`;
+	}
+
+	remove(id: number) {
+		return `This action removes a #${id} product`;
+	}
 }
