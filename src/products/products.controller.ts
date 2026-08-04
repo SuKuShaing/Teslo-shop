195b813eb42c607bc1d9ce37bda558a9ae257be6
Sebastian <sebastian.sanhueza.t@usach.cl>
2026-08-04 13:25:04 -0400

    Actualizamos el las características de un producto, patch

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -35,10 +35,10 @@ export class ProductsController {
 
 	@Patch(':id')
 	update(
-		@Param('id') id: string,
+		@Param('id', ParseUUIDPipe) id: string,
 		@Body() updateProductDto: UpdateProductDto,
 	) {
-		return this.productsService.update(+id, updateProductDto);
+		return this.productsService.update(id, updateProductDto);
 	}
 
 	@Delete(':terminoDeBusqueda')
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,6 +3,7 @@ import {
 	Injectable,
 	InternalServerErrorException,
 	Logger,
+	NotFoundException,
 } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -77,8 +78,24 @@ export class ProductsService {
 		return product;
 	}
 
-	update(id: number, updateProductDto: UpdateProductDto) {
-		return `This action updates a #${id} product`;
+	async update(id: string, updateProductDto: UpdateProductDto) {
+		// Preload no actualiza, simplemente prepara para la actualización
+		const product = await this.productRepository.preload({
+			id: id,
+			...updateProductDto,
+		});
+
+		if (!product)
+			throw new NotFoundException(`Product with id: ${id} not found`);
+
+		try {
+			// Save guarda el producto precargado
+			await this.productRepository.save(product);
+
+			return product;
+		} catch (error) {
+			this.handleDBExceptions(error);
+		}
 	}
 
 	async remove(terminoDeBusqueda: string) {
