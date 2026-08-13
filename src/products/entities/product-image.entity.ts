4a4d169c4128099c9d016aac5d9116504b5da47b
Sebastian <sebastian.sanhueza.t@usach.cl>
2026-08-13 13:36:32 -0400

    Cambiar el nombre de las tablas en la base de datos

--- src/products/entities/product-image.entity.ts
+++ src/products/entities/product-image.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Product } from './product.entity';
 
-@Entity()
+@Entity({ name: 'product_images' }) // sí se le cambia el nombre a tabla, hay que eliminar la anterior en la db, a veces hay que matar el docker que tiene la db, el volumen permanece y volver a levantarlo
 export class ProductImage {
 	@PrimaryGeneratedColumn()
 	id!: number;
