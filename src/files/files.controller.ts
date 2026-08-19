7d3c5a4bcfab2a7ffd3108fd432ff356fd6832c6
Sebastian <sebastian.sanhueza.t@usach.cl>
2026-08-19 14:10:36 -0400

    validaciones en la recepción de imágenes

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,3 +1,4 @@
+import 'multer';
 import {
 	Controller,
 	Get,
@@ -8,17 +9,38 @@ import {
 	Delete,
 	UploadedFile,
 	UseInterceptors,
+	BadRequestException,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
 
 @Controller('files')
 export class FilesController {
 	constructor(private readonly filesService: FilesService) {}
 
 	@Post('product')
-	@UseInterceptors(FileInterceptor('file')) // file es el nombre de la key en el form-data
+	@UseInterceptors(
+		FileInterceptor('file', {
+			// 'file' es el nombre de la key en el form-data
+			fileFilter: fileFilter,
+			// fileFilter es el filtro que colocamos, en este caso validamos que sea una imagen
+		}),
+	)
 	uploadProductImage(@UploadedFile() file: Express.Multer.File) {
-		return file;
+		if (!file) {
+			// sí no es una imagen, no pasa el filtro y aquí no llega ninguna imagen
+			throw new BadRequestException(
+				'Make suere that the file is an image',
+			);
+		}
+
+		console.log({ fileInController: file });
+
+		return {
+			fieldname: file.fieldname,
+			mimetype: file.mimetype,
+			originalname: file.originalname,
+		};
 	}
 }
---
+++ src/files/helpers/fileFilter.helper.ts
@@ -0,0 +1,28 @@
+import 'multer';
+
+export const fileFilter = (
+	req: Express.Request,
+	file: Express.Multer.File,
+	callback: Function,
+) => {
+	console.log({ file });
+	// {
+	//     file: {
+	//         fieldname: 'file',
+	//         originalname: 'MindValley Workbook.pdf',
+	//         encoding: '7bit',
+	//         mimetype: 'application/pdf'
+	//     }
+	// }
+	if (!file) return callback(new Error('File is empty'), false);
+
+	const fileExtension = file.mimetype.split('/')[1];
+	const validExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+	// esto es para aceptar o no un archivo, no lanza excepciones de nest
+
+	if (validExtensions.includes(fileExtension)) {
+		return callback(null, true);
+	}
+
+	callback(null, false);
+};
