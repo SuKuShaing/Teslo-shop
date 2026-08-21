4482ebb766fb765f8682792f1e743f075e719a6b
Sebastian <sebastian.sanhueza.t@usach.cl>
2026-08-21 13:43:28 -0400

    Servir una archivo de manera controlada

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -10,15 +10,31 @@ import {
 	UploadedFile,
 	UseInterceptors,
 	BadRequestException,
+	Res,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { fileFilter, fileNamer } from './helpers';
+import type { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
 	constructor(private readonly filesService: FilesService) {}
+
+	@Get('product/:imageName')
+	findProductImage(
+		@Res() res: Response, // Ahora nest no responde, yo manualmente voy a emitir la respuesta
+		@Param('imageName') imageName: string,
+	) {
+		const path = this.filesService.getStaticProductImage(imageName);
+
+		res.sendFile(path);
+		// res.status(403).json({
+		// 	ok: false,
+		// 	path: path,
+		// });
+	}
 
 	@Post('product')
 	@UseInterceptors(
@@ -26,7 +42,7 @@ export class FilesController {
 			// 'file' es el nombre de la key en el form-data
 			fileFilter: fileFilter,
 			// fileFilter es el filtro que colocamos, en este caso validamos que sea una imagen
-			// limits: { fileSize: 10000000,  }, // valor en bytes, aquí hay 10 MB
+			// limits: { fileSize: 10000000 }, // valor en bytes, aquí hay 10 MB
 			storage: diskStorage({
 				destination: './static/products',
 				filename: fileNamer,
@@ -41,13 +57,14 @@ export class FilesController {
 			);
 		}
 
+		const secureUrl = `${file.filename}`;
+
 		// console.log({ fileInController: file });
+		// fieldname: file.fieldname,
+		// mimetype: file.mimetype,
+		// originalname: file.originalname,
 		console.log(file);
 
-		return {
-			fieldname: file.fieldname,
-			mimetype: file.mimetype,
-			originalname: file.originalname,
-		};
+		return { secureUrl };
 	}
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { join } from 'path';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { existsSync } from 'fs';
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+	getStaticProductImage(imageName: string) {
+		const path = join(__dirname, '../../static/products', imageName); // con esto se crea el path físico del servidor
+
+		if (!existsSync(path))
+			throw new BadRequestException(
+				`No product found with image ${imageName}`,
+			);
+
+		return path;
+	}
+}
