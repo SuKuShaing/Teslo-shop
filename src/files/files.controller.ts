bcde588bd5276f870c877af3e6cc67b74ebdbc61
Sebastian <sebastian.sanhueza.t@usach.cl>
2026-08-21 14:23:00 -0400

    Retornamos un URL usable y compartible sin exponer nuestro arquitectura del servidor

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -17,10 +17,14 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { fileFilter, fileNamer } from './helpers';
 import type { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-	constructor(private readonly filesService: FilesService) {}
+	constructor(
+		private readonly filesService: FilesService,
+		private readonly configService: ConfigService, // da acceso a las variables de entorno
+	) {}
 
 	@Get('product/:imageName')
 	findProductImage(
@@ -57,7 +61,8 @@ export class FilesController {
 			);
 		}
 
-		const secureUrl = `${file.filename}`;
+		// const secureUrl = `${file.filename}`;
+		const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
 
 		// console.log({ fileInController: file });
 		// fieldname: file.fieldname,
