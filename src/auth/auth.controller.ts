ded05375551914f5ebf5fa728c44a95a58daf646
Sebastian <sebastian.sanhueza.t@usach.cl>
2026-09-07 13:25:50 -0300

    Custom Propety Decorators

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,10 +7,13 @@ import {
 	Param,
 	Delete,
 	UseGuards,
+	Req,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './dcorators/get-user.decorator';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -28,10 +31,14 @@ export class AuthController {
 
 	@Get('private')
 	@UseGuards(AuthGuard())
-	testingPrivateRoute() {
+	testingPrivateRoute(
+		// @Req() request: Express.Request // con un Decorador de parámetro com @GetUser nos evitamos repetir request.user en todos los endpoints protegidos, centralizamos la lógica y queda más limpio y directo
+		@GetUser(['email', 'role', 'fullName']) user: User,
+	) {
 		return {
 			ok: true,
 			message: 'Hola Mundo Private',
+			user,
 		};
 	}
 }
